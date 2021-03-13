7b392f02761c294cb6d0f3f696ff154a0722eacd
Ivan Domingos Moreira <idm@br.ibm.com>
2021-03-13 17:33:06 -0300

    organização do código

--- src/main/java/br/com/techschool/EstudandoApplication.java
+++ src/main/java/br/com/techschool/EstudandoApplication.java
@@ -1,5 +1,8 @@
 package br.com.techschool;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstudandoApplication {
 
 	public static void main(String[] args) {
+		
+        Logger logger = LoggerFactory.getLogger(EstudandoApplication.class);
+        logger.info("Starting TechSchool");
 		SpringApplication.run(EstudandoApplication.class, args);
+		
+		logger.info("TechSchool Started");
 	}
 
 }
--- src/main/java/br/com/techschool/controller/LoginController.java
+++ src/main/java/br/com/techschool/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import br.com.techschool.modelo.Usuario;
 import br.com.techschool.persistencia.UsuarioRepository;
 
 @Controller
@@ -26,6 +27,20 @@ public class LoginController {
 	public String logar(@RequestParam("usuario") String usuario,@RequestParam("senha") String senha ) {
 		
 		System.out.println("Usuario: " + usuario + " Senha: " + senha );
+		
+		return "login";
+	}
+		
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String cadastrar (@RequestParam("nome") String nome,@RequestParam("emailUsuario") String emailUsuario,
+							@RequestParam("senha") String senha ) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+		usuario.setEmail(emailUsuario);
+		usuario.setSenha(senha);
+		usuarioRepository.save(usuario);
+		
 		
 		return "login";
 	}
